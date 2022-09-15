f3d3c73be40968f2b62a7d3efc809c1f3d2ca918
Artur Poniatowski <poniat.ap@gmail.com>
2022-09-15 08:43:00 +0300

    Added our sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+const Shop = () => {
+    return (
+        <Fragment>
+            <h1>Welcome in my shop</h1>
+        </Fragment>
+    )
+}
+
+export default Shop;
